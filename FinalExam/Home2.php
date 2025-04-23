e37efa37923844e63b435e71937f409a44260170
midas0502 <hoanglevietanh52@gmail.com>
2025-04-24 05:30:41 +0700

    init project

--- FinalExam/Home2.php
+++ FinalExam/Home2.php
@@ -9,7 +9,7 @@ ini_set('display_errors', 0);
   <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/baitap5.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="C:\xampp\htdocs\QuanLyTrangThietBi\Duc\format1.css">
+    <link rel="stylesheet" href="C:\xampp\htdocs\QuanLyTrangThietBi\Linh\format1.css">
   <title>Document</title>
   <style>
     .thanhtren{
@@ -31,8 +31,8 @@ require_once 'C:\xampp\htdocs\QuanLyTrangThietBi\menutrai2.php'
 <div style="display: inline-block; vertical-align: top; flex:1">
   <div class="thanhtren">
     <div>
-      <a href="http://localhost/QuanLyTrangThietBi/Tam/FinalExam/Home2.php" style="color: rgb(0, 172, 0); width:fit-content; display: inline-block; font-weight: bold; font-size:120%">DANH SÁCH THIẾT BỊ</a>
-      <a href="http://localhost/QuanLyTrangThietBi/Tam/qlkhophong/Home2.php" style="color: rgb(200,200,200); width:fit-content; display: inline-block">DANH SÁCH KHO PHÒNG</a>
+      <a href="http://localhost/QuanLyTrangThietBi/VietAnh/FinalExam/Home2.php" style="color: rgb(0, 172, 0); width:fit-content; display: inline-block; font-weight: bold; font-size:120%">DANH SÁCH THIẾT BỊ</a>
+      <a href="http://localhost/QuanLyTrangThietBi/VietAnh/qlkhophong/Home2.php" style="color: rgb(200,200,200); width:fit-content; display: inline-block">DANH SÁCH KHO PHÒNG</a>
     </div>
       <a href="http://localhost/QuanLyTrangThietBi/login/login.php" style="color: gray; display: inline-block;" onclick="return confirm('Bạn có muốn đăng xuất không')">Đăng xuất</a>
   </div>
--- qlkhophong/Home.php
+++ qlkhophong/Home.php
@@ -9,7 +9,7 @@ ini_set('display_errors', 0);
   <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/baitap5.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="C:\xampp\htdocs\QuanLyTrangThietBi\Duc\format1.css">
+    <link rel="stylesheet" href="C:\xampp\htdocs\QuanLyTrangThietBi\Linh\format1.css">
   <title>Document</title>
   <style>
     .thanhtren{
@@ -58,8 +58,8 @@ require_once 'C:\xampp\htdocs\QuanLyTrangThietBi\menutrai.php'
 <div style="display: inline-block; vertical-align: top; flex:1">
   <div class="thanhtren">
     <div>
-      <a href="http://localhost/QuanLyTrangThietBi/Tam/qlkhophong/Home.php" style="color: rgb(0, 172, 0); width:fit-content; display: inline-block; font-weight: bold; font-size:120%">QUẢN LÝ KHO PHÒNG</a>
-      <a href="http://localhost/QuanLyTrangThietBi/Tam/FinalExam/Home.php"style="color: rgb(200,200,200); width:fit-content; display: inline-block" >QUẢN LÝ THIẾT BỊ</a> 
+      <a href="http://localhost/QuanLyTrangThietBi/VietAnh/qlkhophong/Home.php" style="color: rgb(0, 172, 0); width:fit-content; display: inline-block; font-weight: bold; font-size:120%">QUẢN LÝ KHO PHÒNG</a>
+      <a href="http://localhost/QuanLyTrangThietBi/VietAnh/FinalExam/Home.php"style="color: rgb(200,200,200); width:fit-content; display: inline-block" >QUẢN LÝ THIẾT BỊ</a> 
     </div>
       <a href="http://localhost/QuanLyTrangThietBi/login/login.php" style="color: gray; display: inline-block;" onclick="return confirm('Bạn có muốn đăng xuất không')">Đăng xuất</a>
   </div>
